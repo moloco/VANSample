01917cc9676576fae1ef377e6da86210eeb2f5ba
hyeonseo <hyeonseo@molocoads.com>
2016-02-29 22:25:33 -0800

    VANSample changes to comply with VAN SDK 1.0.3
    Summary: Main change for this sample app is removal of GetEvent API
    
    Test Plan: Tested with Nexus 5 emulator.
    
    Reviewers: sehyuk, ethan, jeon
    
    Reviewed By: ethan, jeon
    
    Subscribers: dev
    
    Maniphest Tasks: T316
    
    Differential Revision: https://moloco.phacility.com/D938

--- android/VanSampleApp/app/src/main/java/com/moloco/vansampleapp/MainActivity.java
+++ android/VanSampleApp/app/src/main/java/com/moloco/vansampleapp/MainActivity.java
@@ -1,43 +1,57 @@
 package com.moloco.vansampleapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.moloco.van.context.MolocoEntryPoint;
+import com.moloco.van.listeners.ApiCallback;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		//Initialize Moloco VAN SDK library.
-		MolocoEntryPoint.init(this, "moloco");
-	}
+        // Send one event before initializing MolocoEntryPoint. This is not actually sent to VAN
+        // server until the library is initialized by {@link MolocoEntryPoint#init(Context, String)}.
+        MolocoEntryPoint.sendEvent("beforeInitEvent", new HashMap<String, Object>(), new ApiCallback<String>() {
+            @Override
+            public void handleResponse(String response) {
+                Log.d("com.moloco.vansample", String.format(
+                        "Got response from event sent before initialization : %s", response));
+            }
+        });
 
+        // Initialize Moloco VAN SDK library.
+        // The second parameter must be a unique string for each app.
+        MolocoEntryPoint.init(this, "MolocoVanSample");
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
 
-		return super.onOptionsItemSelected(item);
-	}
+        return super.onOptionsItemSelected(item);
+    }
 }
--- android/VanSampleApp/app/src/main/java/com/moloco/vansampleapp/MainActivityFragment.java
+++ android/VanSampleApp/app/src/main/java/com/moloco/vansampleapp/MainActivityFragment.java
@@ -1,7 +1,7 @@
 package com.moloco.vansampleapp;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,60 +10,62 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.moloco.van.context.MolocoContext;
 import com.moloco.van.context.MolocoEntryPoint;
 import com.moloco.van.listeners.ApiCallback;
-
-import org.json.JSONObject;
 
 import java.util.HashMap;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment implements View.OnClickListener, ApiCallback<JSONObject> {
+public class MainActivityFragment extends Fragment implements View.OnClickListener, ApiCallback<String> {
 
-	public MainActivityFragment() {
-	}
+    public MainActivityFragment() {
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-	                         Bundle savedInstanceState) {
-		View v = inflater.inflate(R.layout.fragment_main, container, false);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_main, container, false);
 
-		Button post_btn = (Button)v.findViewById(R.id.post_button);
-		post_btn.setOnClickListener(this);
+        Button post_btn = (Button) v.findViewById(R.id.send_button);
+        post_btn.setOnClickListener(this);
 
-		Button get_btn = (Button)v.findViewById(R.id.get_button);
-		get_btn.setOnClickListener(this);
+        TextView textView = (TextView) v.findViewById(R.id.textView);
+        textView.setMovementMethod(new ScrollingMovementMethod());
 
-		TextView textView = (TextView)v.findViewById(R.id.textView);
-		textView.setMovementMethod(new ScrollingMovementMethod());
+        return v;
+    }
 
-		return v;
-	}
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.send_button: {
+                    HashMap<String, Object> dataMap = new HashMap<String, Object>();
+                    dataMap.put("username", "moloco");
+                    dataMap.put("latitude", 0.123923f);
+                    dataMap.put("longitude", 0.5346123f);
+                    MolocoEntryPoint.sendEvent("clickEvent", dataMap, MainActivityFragment.this);
+                }
+                break;
+            default:
+                break;
+        }
+    }
 
-	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
-			case R.id.post_button: {
-				HashMap<String, Object> dataMap = new HashMap<String, Object>();
-				dataMap.put("username", "Ethan");
-				dataMap.put("latitude", 0.123923f);
-				dataMap.put("longitude", 0.5346123f);
-				MolocoEntryPoint.SendEvent("test", dataMap, MainActivityFragment.this);
-			} break;
-			case R.id.get_button: {
-				MolocoEntryPoint.GetEvent("test", MainActivityFragment.this);
-			} break;
-			default:
-				break;
-		}
-	}
+    @Override
+    public void handleResponse(String jsonObject) {
+        TextView textView = (TextView) getView().findViewById(R.id.textView);
+        textView.setText(String.format("Result: %s", jsonObject));
+        Log.d("com.moloco.vansample", String.format("Result for click event : %s", jsonObject));
 
-	@Override
-	public void handleResponse(JSONObject jsonObject) {
-		TextView textView = (TextView) getView().findViewById(R.id.textView);
-		textView.setText(String.format("%s", jsonObject));
-		Log.d("com.moloco.vansample", String.format("Get JSON : %s", jsonObject));
-	}
+        Log.d("com.moloco.vansample", String.format(
+                "Device Info\nIDFA : %s\nModel : %s\nLocation : %s\nCarrier : %s",
+                MolocoContext.INSTANCE.getAdvertisingId(),
+                MolocoContext.INSTANCE.getDeviceInfo(),
+                MolocoContext.INSTANCE.getLocation(),
+                MolocoContext.INSTANCE.getCarrier()
+        ));
+    }
 }
